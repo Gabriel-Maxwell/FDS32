49abcebac34fb4ea329b36208de4ba7bbbd10a89
Gabriel Maxwell <103340783+Gabriel-Maxwell@users.noreply.github.com>
2025-08-08 00:34:34 +0000

    primeira alteracao na branch alteracao

--- Class08.05.25/src/App.java
+++ Class08.05.25/src/App.java
@@ -3,5 +3,7 @@ public class App {
         System.out.println("Hello, World!");
         //comment: This is a simple Java application that prints a message to the console.
         // You can run this application using the command: java App
+        // i'm using a new branch for this change
+        // This is a new line added to the main method.
     }
 }
