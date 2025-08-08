b04e521b9ef8d20044f0593d9621da026fb2c2e6
Gabriel Maxwell <103340783+Gabriel-Maxwell@users.noreply.github.com>
2025-08-08 00:45:34 +0000

    alteracao completa

--- Class08.05.25/src/App.java
+++ Class08.05.25/src/App.java
@@ -1,9 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, i made a series of alterations on this code!");
         //comment: This is a simple Java application that prints a message to the console.
         // You can run this application using the command: java App
         // i'm using a new branch for this change
         // This is a new line added to the main method.
+        // alteration to the main method made in the new branch
     }
 }
