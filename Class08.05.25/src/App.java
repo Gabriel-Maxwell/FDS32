ce245befa409c7338172c22dce8c41f68a4e95d9
Gabriel Maxwell <103340783+Gabriel-Maxwell@users.noreply.github.com>
2025-08-08 00:31:00 +0000

    Alteracao 1 no main

--- Class08.05.25/src/App.java
+++ Class08.05.25/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //comment: This is a simple Java application that prints a message to the console.
+        // You can run this application using the command: java App
     }
 }
